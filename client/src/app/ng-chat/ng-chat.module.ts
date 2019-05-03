ffe0081a9078c7042cc8b11b2c19fba0365adde3
Matias Pierobon <matias.pierobonm@gmail.com>
2019-05-03 11:11:15 -0300

    generate chat component

--- client/src/app/ng-chat/ng-chat.module.ts
+++ client/src/app/ng-chat/ng-chat.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ChatComponent],
   imports: [
     CommonModule
   ]
